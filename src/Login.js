181eb0e6b66e563df2f41cb03e49f06fdb77a456
Mathew <mmacelwee89@gmail.com>
2020-09-05 17:20:56 -0700

    initial commit

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className= 'login'>
+            <h1>this is the login page</h1>
+            {/* spotify logo */}
+            {/* login with spotify button */}
+        </div>
+    )
+}
+
+export default Login
